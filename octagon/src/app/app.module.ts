50495b3c1d54f91be172a69cdae4de27105fb7f9
Henry Barnacle <henrybarnacle@yahoo.com>
2018-01-03 13:28:45 -0500

    adding home and contact components

--- octagon/src/app/app.module.ts
+++ octagon/src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ContactComponent } from './components/contact/contact.component';
 
+//create routes
+const appRoutes: Routes = [
+    {path:'', component: HomeComponent},
+    {path: 'contact', component: ContactComponent},
+  
+
+
+    ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentsComponent,
+    HomeComponent,
+    ContactComponent,
+ 
+  
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
+
   ],
   providers: [],
   bootstrap: [AppComponent]
